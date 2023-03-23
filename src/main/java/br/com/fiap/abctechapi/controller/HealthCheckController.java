f9743b063355b1eccae08784b4436405c97a595d
Luiz Zenha <lzenha@gmail.com>
2023-03-23 20:41:04 -0300

    Configuracoes

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
@@ -12,6 +16,17 @@ public class HealthCheckController {
     @GetMapping
     public ResponseEntity<String> status(){
         return ResponseEntity.ok("UP");
+    }
+
+    @GetMapping("version")
+    public ResponseEntity<String> version() throws IOException {
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
+
+        properties.load(inputStream);
+
+        return ResponseEntity.ok(properties.get("build_name") + " - " + properties.get("build_version"));
+
     }
 
 }
