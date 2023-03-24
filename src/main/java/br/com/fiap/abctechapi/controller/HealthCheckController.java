987fab912d148df7a2f92fbc2e99ffe5f632d032
Luiz Zenha <lzenha@gmail.com>
2023-03-23 21:43:58 -0300

    Version component

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package br.com.fiap.abctechapi.controller;
 
+import br.com.fiap.abctechapi.util.VersionComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,20 +15,20 @@ import java.util.Properties;
 @RequestMapping("/")
 public class HealthCheckController {
 
+    private final VersionComponent versionComponent;
+
+    public HealthCheckController(@Autowired VersionComponent versionComponent){
+        this.versionComponent = versionComponent;
+    }
+
     @GetMapping
     public ResponseEntity<String> status(){
         return ResponseEntity.ok("UP");
     }
 
     @GetMapping("version")
-    public ResponseEntity<String> version() throws IOException {
-        Properties properties = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
-
-        properties.load(inputStream);
-
-        return ResponseEntity.ok(properties.get("build_name") + " - " + properties.get("build_version"));
-
+    public ResponseEntity<String> version(){
+        return ResponseEntity.ok(this.versionComponent.getVersion());
     }
 
 }
---
+++ src/main/java/br/com/fiap/abctechapi/util/VersionComponent.java
@@ -0,0 +1,24 @@
+package br.com.fiap.abctechapi.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Component
+public class VersionComponent {
+
+    private final Properties properties;
+
+    public VersionComponent() throws IOException {
+        properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
+        properties.load(inputStream);
+    }
+
+    public String getVersion(){
+        return properties.get("build_name") + " - " + properties.get("build_version");
+    }
+}
